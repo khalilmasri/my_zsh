a17ac967648dd0ec4b8a988380555f38b7bdc2dd
khalilmasri <khalil.1996km@gmail.com>
2021-11-10 17:21:04 +0200

    main

--- src/main.c
+++ src/main.c
@@ -20,17 +20,28 @@
 
 int main(void){
 
-    char *std_in;
+    char *std_in = "\0";
+    status_t status = 0;
     argument *table = malloc(sizeof(argument));
 
-    while(my_strcmp(std_in, "exit") != 0){
+    char current_dir[MAX_STR_LEN];
+    getcwd(current_dir, MAX_STR_LEN);
+    my_putstr(current_dir);
+    my_putstr("\n");
+
+    while(status != QUIT){
         write(0, "> ", my_strlen("> "));
         std_in = my_readline();
         table->size = arg_count(std_in); 
         table = parse_input(table,std_in);
+        status = execute_args(table);
+
+        free(std_in);
     }
-    
+        
     free_argument(table); 
+    free(table);
+    
     return 0;
 }
 
