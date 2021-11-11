85f8e07ab735f316ead240147e86ecddf1e17b30
khalilmasri <khalil.1996km@gmail.com>
2021-11-11 17:12:33 +0200

    echo

--- src/echo.c
+++ src/echo.c
@@ -41,10 +41,81 @@ status_t write_to_file(argument* table, int where){
     return SUCCESS;
 }
 
+char *cut_dollar(char *str){
+
+    int i = 1;
+
+    while(str[i]){
+        str[i-1] = str[i];
+        i++;
+    }
+
+    str[i-1] = '\0';
+
+    return str;
+}
+
+char *cut_variable(char *str){
+
+    int index = 0,
+        cur   = 0;
+
+    char *ret = malloc(sizeof(char)*MAX_STR_LEN);
+
+    while(str[index] != '=')
+        index++;
+
+    index++;
+    while(str[index]){
+        if(str[index] != ':'){
+            ret[cur] = str[index];
+            cur++;
+        }else{
+            ret[cur] = ' ';
+            cur++;
+        }
+        index++;
+    }
+
+    ret[cur] = '\0';
+
+    return ret;
+}
+
+status_t find_in_env(argument *table){
+
+    if(table->size > 2){
+        my_putstr("Can check one variable in ENV at a time.\n");
+        return ERROR;
+    }
+
+    table->args[1] = cut_dollar(table->args[1]);
+
+    int index = 0;
+
+    while(index < table->env_length){
+        if(check_variable_exist(table->args[1], table->env[index]) == 1){
+            printf("Here\n");
+            my_putstr(cut_variable(table->env[index]));
+            my_putstr("\n");
+            return SUCCESS;
+        }
+        index++;
+    }
+
+    my_putstr("No match found.");
+    my_putstr("\n");
+    return ERROR;
+}
+
 status_t echo(argument *table){
 
     int where = check_insert_file(table),
         index = 1;
+
+    if(table->args[1][0] == '$'){
+        return find_in_env(table); 
+    }
 
     if(where == 0){
         while(table->args[index]){
