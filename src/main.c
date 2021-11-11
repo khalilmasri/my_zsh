7c25fdea3c92e813d4c0117809d15069bd932bc4
khalilmasri <khalil.1996km@gmail.com>
2021-11-11 12:10:57 +0200

    main

--- src/main.c
+++ src/main.c
@@ -29,6 +29,7 @@ int main(int argc, char *argv[], char **envi){
     char *std_in = "\0";
     status_t status = 0;
     argument *table = malloc(sizeof(argument));
+    table->env = get_env(table,envi);
 
     print_current_dir();
 
@@ -37,7 +38,6 @@ int main(int argc, char *argv[], char **envi){
         std_in = my_readline();
         table->size = arg_count(std_in); 
         table = parse_input(table,std_in);
-        table->env = get_env(table,envi);
         status = execute_args(table);
 
         free(std_in);
