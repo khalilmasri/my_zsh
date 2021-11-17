47510534543241ecf3df1d29487029416796fd99
khalilmasri <khalil.1996km@gmail.com>
2021-11-17 19:36:21 +0200

    cd

--- src/cd.c
+++ src/cd.c
@@ -1,21 +1,46 @@
 #include "../include/include.h"
 
+char **get_home(argument *table){
+
+    free_argument(table);
+    table->size = 3;
+    table->args = malloc(sizeof(char*)*table->size);
+    int index = 0;
+
+    table->args[0] = malloc(sizeof(char)*3);
+    my_strcpy(table->args[0], "cd");
+
+    while (index < table->env_length){
+        if (check_variable_exist("HOME", table->env[index]) == 1){
+            table->args[1] = cut_variable(table->env[index]);
+            break;
+        }
+        index++;
+    }
+    table->args[2] = NULL;
+
+    return table->args;
+}
+
 status_t cd(argument *table)
 {
 
     char cwd[MAX_STR_LEN];
-    char current_dir[MAX_STR_LEN];
     static char prev_dir[MAX_STR_LEN];
+
+    if(!table->args[1] || !my_strcmp(table->args[1], "~") ){
+        table->args = get_home(table);
+    }
 
     if (!my_strcmp(table->args[1], "-"))
     {
         chdir(prev_dir);
-        getcwd(current_dir, MAX_STR_LEN);
-        my_putstr(current_dir);
-        my_putstr("\n");
+        print_current_dir();
         return SUCCESS;
     }
     getcwd(prev_dir, MAX_STR_LEN);
+
+
     DIR *dir = opendir(table->args[1]);
 
     if (!dir)
@@ -34,9 +59,8 @@ status_t cd(argument *table)
     else
         chdir(table->args[1]);
 
-    getcwd(current_dir, MAX_STR_LEN);
-    my_putstr(current_dir);
-    my_putstr("\n");
+    print_current_dir();
+    closedir(dir);
 
     return SUCCESS;
 }
